6ea8ba4060c012c2c6924d2f7d163ad7df4bf4ed
Ka Wing Tsang <kawingt2@gmail.com>
2021-04-01 11:28:05 +0800

    sort and filter file name and optimise page style

--- app.js
+++ app.js
@@ -10,20 +10,41 @@ const port = 8000;
 
 // Getting all the files and their urls
 const files = fs.readdirSync(dirPath).map(name => {
+    let id = name[0];
+    if(name[1] !== ' ') {
+        id += name[1]
+    }
     return {
+        id: id,
         name: path.basename(name, '.pdf'),
         url: `/notes/${name}`
     };
 });
 
-// // Test code
 // console.log(files);
+
+// Compare id 
+function compareID(a, b) {
+    // turn the id into integer and compare
+    return parseInt(a.id) - parseInt(b.id);
+}
+// sort the array in ascending order
+files.sort(compareID);
+
+// console.log(files);
+
+// Filtering out DS_store file
+const filteredFiles = files.filter(file => file.id !== '.D');
+
+console.log(filteredFiles);
+
+
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.render("index", {files})
+    res.render("index", {filteredFiles})
 });
 
 
