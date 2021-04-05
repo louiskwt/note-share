a36416c107f0eb4d0a69caf734b58df31809e84d
Ka Wing Tsang <kawingt2@gmail.com>
2021-04-05 17:37:41 +0800

    updated notes and improved ui

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const path = require('path');
 const fs = require('fs');
 
 const app = express();
-const dirPath = path.join(__dirname, 'public/notes');
 const PORT = process.env.PORT || 8000;
 
 // Getting all the files and their urls
-const files = fs.readdirSync(dirPath).map(name => {
+const P1_dirPath = path.join(__dirname, 'public/p1_notes');
+const P1_files = fs.readdirSync(P1_dirPath).map(name => {
     let id = name[0];
     if(name[1] !== ' ') {
         id += name[1]
@@ -16,24 +16,56 @@ const files = fs.readdirSync(dirPath).map(name => {
     return {
         id: id,
         name: path.basename(name, '.pdf'),
-        url: `/notes/${name}`
+        url: `/p1_notes/${name}`
     };
 });
 
-// console.log(files);
+const P2_dirPath = path.join(__dirname, 'public/p2_notes');
+const P2_files = fs.readdirSync(P2_dirPath).map(name => {
+    let id = name[0];
+    if(name[1] !== ' ') {
+        id += name[1]
+    }
+    return {
+        id: id,
+        name: path.basename(name, '.pdf'),
+        url: `/p2_notes/${name}`
+    };
+});
+
+const P3_dirPath = path.join(__dirname, 'public/p3_notes');
+const P3_files = fs.readdirSync(P3_dirPath).map(name => {
+    let id = name[0];
+    if(name[1] !== ' ') {
+        id += name[1]
+    }
+    return {
+        id: id,
+        name: path.basename(name, '.pdf'),
+        url: `/p3_notes/${name}`
+    };
+});
+
+
+
 
 // Compare id 
 function compareID(a, b) {
     // turn the id into integer and compare
     return parseInt(a.id) - parseInt(b.id);
 }
+
 // sort the array in ascending order
-files.sort(compareID);
+P1_files.sort(compareID);
+P2_files.sort(compareID);
+P3_files.sort(compareID);
 
 // console.log(files);
 
 // Filtering out DS_store file
-const filteredFiles = files.filter(file => file.id !== '.D');
+const filteredP1_Files = P1_files.filter(file => file.id !== '.D');
+const filteredP2_Files = P2_files.filter(file => file.id !== '.D');
+const filteredP3_Files = P3_files.filter(file => file.id !== '.D');
 
 
 
@@ -42,7 +74,7 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.render("index", {filteredFiles});
+    res.render("index", {filteredP1_Files, filteredP2_Files, filteredP3_Files});
 });
 
 
--- public/js/ui.js
+++ public/js/ui.js
@@ -1,14 +1,17 @@
 // Expandable list for note section
-const expandBtn = document.querySelector('.expand-btn'); 
+const expandBtnArr = Array.from(document.querySelectorAll('.expand-btn')); 
 
 // Expand Btn event handler
-expandBtn.addEventListener('click', (e) => {
-    // find tha parent to get the id name
-    const paperType = e.target.parentNode.parentNode.id;
-    // use the id name to target the corresponding list
-    if(document.getElementById(`${paperType}-list`).classList.contains('d-none')) {
-        document.getElementById(`${paperType}-list`).classList.remove('d-none');
-    } else {
-        document.getElementById(`${paperType}-list`).classList.add('d-none');
-    }
+expandBtnArr.forEach((expandBtn) => {
+    expandBtn.addEventListener('click', (e) => {
+        // find tha parent to get the id name
+        const paperType = e.target.parentNode.parentNode.id;
+        console.log(paperType);
+        // use the id name to target the corresponding list
+        if(document.getElementById(`${paperType}-list`).classList.contains('d-none')) {
+            document.getElementById(`${paperType}-list`).classList.remove('d-none');
+        } else {
+            document.getElementById(`${paperType}-list`).classList.add('d-none');
+        }
+    })
 })
