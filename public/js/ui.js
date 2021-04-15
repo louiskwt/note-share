f3dbfd4a1460bec4f6442b36a3d5774eed8a7d45
Ka Wing Tsang <kawingt2@gmail.com>
2021-04-15 11:09:53 +0800

    iupdated nav ui

--- public/js/ui.js
+++ public/js/ui.js
@@ -4,22 +4,23 @@ const expandBtnArr = Array.from(document.querySelectorAll('.expand-btn'));
 // Expand Btn event handler
 expandBtnArr.forEach((expandBtn) => {
 	expandBtn.addEventListener('click', (e) => {
-		// find tha parent to get the id name
-		const paperType = e.target.parentNode.parentNode.id;
-		console.log(paperType);
-		// use the id name to target the corresponding list
-		if (
-			document
-				.getElementById(`${paperType}-list`)
-				.classList.contains('d-none')
-		) {
-			document
-				.getElementById(`${paperType}-list`)
-				.classList.remove('d-none');
-		} else {
-			document
-				.getElementById(`${paperType}-list`)
-				.classList.add('d-none');
-		}
+		expand(e);
 	});
 });
+
+// function for expanding note section
+function expand(e) {
+	// find tha parent to get the id name
+	const paperType = target.parentNode.parentNode.id;
+
+	// use the id name to target the corresponding list
+	if (
+		document
+			.getElementById(`${paperType}-list`)
+			.classList.contains('d-none')
+	) {
+		document.getElementById(`${paperType}-list`).classList.remove('d-none');
+	} else {
+		document.getElementById(`${paperType}-list`).classList.add('d-none');
+	}
+}
